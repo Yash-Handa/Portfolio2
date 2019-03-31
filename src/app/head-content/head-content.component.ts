88d8ed2bf462b4f5e898b8f15fe42b4777e5d4da
Yash Handa <yashhanda7@yahoo.com>
2019-03-31 22:46:31 +0530

    Added lightroom finally (hopefully)

--- src/app/head-content/head-content.component.ts
+++ src/app/head-content/head-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-head-content',
@@ -6,6 +6,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./head-content.component.css', './head-contact.component.css']
 })
 export class HeadContentComponent implements OnInit, AfterViewInit {
+  @ViewChild('lightbox') lightbox: ElementRef;
   size = window.innerWidth;
   mouseEnter($event) {
     $event.target.className = 'animated tada infinite';
@@ -13,19 +14,20 @@ export class HeadContentComponent implements OnInit, AfterViewInit {
   mouseLeave($event) {
     $event.target.className = 'bottom-iconBtn vertical-align-wrapper';
   }
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    document.getElementById('portrait-cover').className += 'animated rotateInDownLeft';
+    document.getElementById('portrait-cover').className += ' animated rotateInDownLeft';
     document.getElementById('circle-cover').className += ' animated bounceInDown';
     document.getElementById('expert').className += ' animated bounceInDown delay-2s';
     document.getElementById('next').className += ' animated bounceInDown delay-2s';
     document.getElementById('expert2').className += ' animated fadeIn delay-1s';
     document.getElementById('content').className = 'animated fadeIn delay-1s';
     document.getElementById('inspired').className = 'animated fadeIn delay-1s';
+    this.renderer.setStyle(this.lightbox.nativeElement, 'display', 'none');
     setTimeout(() => {
       document.getElementById('portrait-cover').className = '';
     }, 1000);
@@ -36,5 +38,10 @@ export class HeadContentComponent implements OnInit, AfterViewInit {
 
   stopReload(e: any) {
     e.preventDefault();
+    if (this.lightbox.nativeElement.style.display === 'none') {
+      this.renderer.setStyle(this.lightbox.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.lightbox.nativeElement, 'display', 'none');
+    }
   }
 }
